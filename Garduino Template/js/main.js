11753340620fb0f62349abe3aea7cd7eab541979
Beth Josey <34348118+bethjosey@users.noreply.github.com>
2018-01-06 12:16:18 -0500

    Changes
    Removed dummy js.

--- Garduino Template/js/main.js
+++ Garduino Template/js/main.js
@@ -2,20 +2,3 @@
 (function() {
 	"use strict";
 console.log("SEAF has fired");	
-
-
-function plantinfo(height) {
-	var plantbutton = document.querySelector("#plantbutton")
-	var plantmessage = "This is plant 1";
-
-	function plantmessage() {
-		alert(plantmessage)
-	}
-
-	plantbutton.addEventListener("click", plantmessage, false);
-	}
-
-	plantinfo();
-
-
-})();
