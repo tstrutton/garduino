d60588959b1147e71584e26d92e187e55391aeb0
j-vandervaart <jvand223@gmail.com>
2018-01-07 13:08:42 -0500

    First round of CSS polishes and JS additions

--- Garduino Template/js/main.js
+++ Garduino Template/js/main.js
@@ -1,4 +1,33 @@
 //self executing anonymous function
 (function() {
 	"use strict";
-console.log("SEAF has fired");	
+	console.log("SEAF has fired");
+
+	var plants = document.querySelectorAll(".plants");
+
+	for (var i = 0; i < plants.length; i++) {
+			plants[i].addEventListener("click", changeOption, false);
+		}	
+
+	function changeOption(e) {	
+		var selectedPlant = document.querySelector("[selected=selected]");
+
+		if(selectedPlant) {
+			//console.log('Making another selection.');
+
+			for (var p = 0; p < plants.length; p++){
+				plants[p].classList.remove("plantClick"); //Loop through and remove any existing visual indicators
+				plants[p].removeAttribute("selected"); //Remove any existing selections
+			}
+
+			document.querySelector("#"+e.currentTarget.id+"Click").setAttribute("selected", "selected"); //Make a new selection
+			document.querySelector("#"+e.currentTarget.id).classList.add("plantClick"); //Indicate the selection visually
+		} else {
+			//console.log('Making first selection.');
+
+			document.querySelector("#"+e.currentTarget.id+"Click").setAttribute("selected", "selected");
+			document.querySelector("#"+e.currentTarget.id).classList.add("plantClick");
+		}
+	}
+
+}());
